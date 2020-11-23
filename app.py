ad13cf38d89afe3b8b743867ef783bcfd742138c
syedmahmud421 <73360730+syedmahmud421@users.noreply.github.com>
2020-11-23 22:45:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name= str(name)  + "Has just entered"
+	new_name= str(name)  + " Has just entered"
 	return render_template('index.html', name=new_name)
 
 
